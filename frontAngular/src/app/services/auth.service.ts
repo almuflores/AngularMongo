590d4c997d1c8ea3f93146eaf5700beca8a6db0d
almuflores <43790828+almuflores@users.noreply.github.com>
2021-03-05 08:58:07 +0100

    Add files via upload

---
+++ frontAngular/src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  url = 'http://localhost:4000/api'
+  constructor(private http: HttpClient, private router: Router) { }
+
+  signUpUser(user){
+    return this.http.post<any>(this.url + '/register', user);
+  }
+
+  signInUser(user){
+    return this.http.post<any>(this.url + '/signin', user);
+  }
+
+  loggedIn(): Boolean{
+    //comprobamos si está loggeado
+    return !!localStorage.getItem('token');
+
+  }
+
+  getToken(){
+    return localStorage.getItem('token');
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/signin'])
+  }
+}
